386225beae2ee0ef960e1731b437b93d6c9716dc
dlwowlb <dlwowlb@yonsei.ac.kr>
2025-02-21 16:26:34 +0900

    initial commit

---
+++ TextTransformer.py
@@ -0,0 +1,11 @@
+from beartype.typing import List, Optional, Tuple
+from beartype import beartype
+
+
+
+
+class textTransformer:
+    @beartype
+    def __init__(self, text):
+
+        super().__init__()
