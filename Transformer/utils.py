9d54ba072af07362882f07e2c128895747b89fa1
dlwowlb <dlwowlb@yonsei.ac.kr>
2025-03-13 13:02:06 +0900

    initial commit

--- Transformer/utils.py
+++ Transformer/utils.py
@@ -191,8 +191,51 @@ def prob_mask_like(shape, prob, device):
     
 
 #gradient scaling
+#t의 forward 결과는 그대로 유지하면서, 역전파 시에 전달되는 gradient의 크기를 조절(감쇠)하는 역할
 def grad_shrink(t, alpha = 0.1):
     return t * alpha + t.detach() * (1 - alpha)
+
+class RelativePositionBias(nn.Module):
+    """ from https://arxiv.org/abs/2111.09883 """
+
+    def __init__(
+        self,
+        *,
+        dim,
+        heads,
+        layers = 3
+    ):
+        super().__init__()
+        self.net = nn.ModuleList([])
+        self.net.append(nn.Sequential(nn.Linear(1, dim), nn.SiLU()))
+
+        for _ in range(layers - 1):
+            self.net.append(nn.Sequential(nn.Linear(dim, dim), nn.SiLU()))
+
+        self.net.append(nn.Linear(dim, heads))
+
+    @property
+    def device(self):
+        return next(self.parameters()).device
+
+    def forward(self, i, j):
+        assert j >= i
+        device = self.device
+
+        i_pos = torch.arange(i, device = device) + (j - i)
+        j_pos = torch.arange(j, device = device)
+
+        rel_pos = (rearrange(i_pos, 'i -> i 1') - rearrange(j_pos, 'j -> 1 j'))
+        rel_pos += (j - 1)
+
+        x = torch.arange(-j + 1, j, device = device).float()
+        x = rearrange(x, '... -> ... 1')
+
+        for layer in self.net:
+            x = layer(x)
+
+        x = x[rel_pos]
+        return rearrange(x, 'i j h -> h i j')
 
 class Transformer(nn.Module):
     def __init__(
